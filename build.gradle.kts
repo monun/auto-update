50c491f937ad9df32f7936542e6fea550d81c73a
monun <monun1010@gmail.com>
2021-05-29 22:59:27 +0900

    :bookmark: 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.OutputStream
 
 plugins {
-    kotlin("jvm") version "1.4.31"
-    kotlin("plugin.serialization") version "1.4.31"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.serialization") version "1.4.32"
     id("com.github.johnrengelman.shadow") version "5.2.0"
-//    `maven-publish`
+    `maven-publish`
 }
-
-val relocate = (findProperty("relocate") as? String)?.toBoolean() ?: true
 
 repositories {
     mavenLocal()
@@ -18,65 +18,83 @@ repositories {
 
 dependencies {
     compileOnly(kotlin("stdlib"))
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+//    compileOnly("com.github.monun:invfx:2.0.0")
 
-    implementation("com.github.monun:tap:3.3.5")
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-//    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-//    testImplementation("org.mockito:mockito-core:3.6.28")
-//    testImplementation("org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT")
+    implementation("com.github.monun:tap:3.6.0")
+    implementation("com.github.monun:kommand:1.0.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.mockito:mockito-core:3.6.28")
+    testImplementation("org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT")
 }
 
+/*//ProtocolLib
+repositories {
+    maven("https://repo.dmulloy2.net/repository/public/")
+}
+dependencies {
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0")
+}*/
+
 tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
     }
+
     processResources {
         filesMatching("**/*.yml") {
             expand(project.properties)
         }
     }
+
     test {
         useJUnitPlatform()
         doLast {
             file("logs").deleteRecursively()
         }
     }
+
     create<Jar>("sourcesJar") {
         from(sourceSets["main"].allSource)
         archiveClassifier.set("sources")
     }
-    shadowJar {
+
+    fun ShadowJar.pluginJar(classifier: String = "") {
         archiveBaseName.set(project.property("pluginName").toString())
         archiveVersion.set("") // For bukkit plugin update
-        archiveClassifier.set("") // Remove 'all'
-
-        if (relocate) {
-            relocate("com.github.monun.kommand", "${rootProject.group}.${rootProject.name}.kommand")
-            relocate("com.github.monun.tap", "${rootProject.group}.${rootProject.name}.tap")
-        }
-
-        doFirst {
-            println("relocate = $relocate")
-        }
+        archiveClassifier.set(classifier)
+        from(sourceSets["main"].output)
+        configurations = listOf(project.configurations.implementation.get().apply { isCanBeResolved = true })
     }
+
+    create<ShadowJar>("pluginJar") {
+        pluginJar()
+        relocate("com.github.monun.kommand", "${rootProject.group}.${rootProject.name}.kommand")
+        relocate("com.github.monun.tap", "${rootProject.group}.${rootProject.name}.tap")
+    }
+
+    create<ShadowJar>("testPluginJar") {
+        pluginJar("TEST")
+    }
+
     build {
-        dependsOn(shadowJar)
+        dependsOn(named("pluginJar"))
     }
+
     create<Copy>("copyToServer") {
-        from(shadowJar)
+        val task = named("testPluginJar")
+        from(task)
         val plugins = File(rootDir, ".server/plugins")
-        if (File(plugins, shadowJar.get().archiveFileName.get()).exists()) {
+        if (File(plugins, (task.get() as ShadowJar).archiveFileName.get()).exists()) {
             into(File(plugins, "update"))
         } else {
             into(plugins)
         }
     }
+
     create<DefaultTask>("setupWorkspace") {
         doLast {
             val versions = arrayOf(
@@ -118,12 +136,12 @@ tasks {
     }
 }
 
-//publishing {
-//    publications {
-//        create<MavenPublication>(project.property("pluginName").toString()) {
-//            artifactId = project.name
-//            from(components["java"])
-//            artifact(tasks["sourcesJar"])
-//        }
-//    }
-//}
+publishing {
+    publications {
+        create<MavenPublication>(project.property("pluginName").toString()) {
+            artifactId = project.name
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+        }
+    }
+}
